9d4e1d6a3b81635205045268c462bdb35edae1ea
Alexandre Vaz de Mello <102325671+alevazdemello@users.noreply.github.com>
2022-11-07 12:41:14 -0300

    manipulandoTexto

---
+++ manipulandoTexto.py
@@ -0,0 +1,22 @@
+frase = 'RuPauls drag Race'
+print(frase[4:8])
+print(frase[4:])
+print(frase[:10])
+print(frase[::2])
+
+print(frase.count('a'))
+
+print("""Para escrever longos textos, recomenda-se o uso das três aspas. 
+Para escrever longos textos, recomenda-se o uso das três aspas. 
+Para escrever longos textos, recomenda-se o uso das três aspas. 
+Para escrever longos textos, recomenda-se o uso das três aspas. 
+Para escrever longos textos, recomenda-se o uso das três aspas. 
+Para escrever longos textos, recomenda-se o uso das três aspas. 
+Para escrever longos textos, recomenda-se o uso das três aspas. 
+Para escrever longos textos, recomenda-se o uso das três aspas. 
+Para escrever longos textos, recomenda-se o uso das três aspas. """)
+
+
+print(frase.upper().count('A'))
+print(len(frase))
+print(frase.split())
